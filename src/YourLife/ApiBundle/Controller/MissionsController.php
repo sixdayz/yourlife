8414cb76f67621d59100f8665a4980b70dbba252
Eduard Fedorov <eduard.fjodorov@tatar.ru>
2013-10-06 03:46:52 +0400

    Доработана работа с миссиями

--- src/YourLife/ApiBundle/Controller/MissionsController.php
+++ src/YourLife/ApiBundle/Controller/MissionsController.php
@@ -6,12 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use YourLife\ApiBundle\Exception\AccessErrorApiException;
 use YourLife\ApiBundle\Exception\ApiException;
+use YourLife\ApiBundle\Exception\MissionNotFoundApiException;
 use YourLife\ApiBundle\Helper\ApiExceptionType;
 use YourLife\ApiBundle\Service\UserService as ApiUserService;
 use YourLife\DataBundle\Document\Mission;
 use YourLife\DataBundle\Document\MissionCloseConditions;
-use YourLife\DataBundle\Document\MissionPhoto;
+use YourLife\DataBundle\Document\MissionResult;
+use YourLife\DataBundle\Document\Photo;
+use YourLife\DataBundle\Service\MissionResultService;
 
 
 class MissionsController extends Controller
@@ -33,16 +37,41 @@ class MissionsController extends Controller
         $request = $this->getRequest();
 
         $user_id = $request->get('user_id');
-        $token = $request->get('token');
+        $token = $request->headers->get('session_token');
 
-        $this->api_service->checkToken($token);
-        $user = $this->api_service->getUser($user_id);
+        $this->api_service->getUserByToken($token);
+        $user = $this->api_service->getUserById($user_id);
 
-        // todo: получить текущие миссии пользователя
+//        todo: получить текущие миссии пользователя
 
-        $repo = $this->getDoctrine()->getRepository('YourLifeDataBundle:Mission');
+        $repo = $this->get('doctrine_mongodb')->getRepository('YourLifeDataBundle:Mission');
         $result = $this->convertMissionsToArray($repo->findAll());
 
+        return new JsonResponse($result, 200);
+    }
+
+    /**
+     * Получаем доступные миссии для пользователя $user_id
+     * */
+    public function getAvailableMissionsAction() {
+        $request = $this->getRequest();
+
+        $user_id = $request->get('user_id');
+        $token = $request->headers->get('session_token');;
+
+        $userByToken = $this->api_service->getUserByToken($token);
+        $userById = $this->api_service->getUserById($user_id);
+
+        if($userById != $userByToken)
+            throw new AccessErrorApiException();
+
+        $repo = $this->get('doctrine_mongodb')->getRepository('YourLifeDataBundle:Mission');
+
+//        $missions = $repo->findBy([
+//            'user_level' => $user->getLevel()
+//        ]);
+
+        $result = $this->convertMissionsToArray($repo->findAll());
         return new JsonResponse($result, 200);
     }
 
@@ -54,63 +83,90 @@ class MissionsController extends Controller
 
         $user_id = $request->get('user_id');
         $mission_id = $request->get('mission_id');
-        $token = $request->get('token');
+        $token = $request->headers->get('session_token');;
 
-        $this->api_service->checkToken($token);
-        $user = $this->api_service->getUser($user_id);
+        $this->api_service->getUserByToken($token);
+        $user = $this->api_service->getUserById($user_id);
 
         // todo: получаем миссию
 
-        $repo = $this->getDoctrine()->getRepository('YourLifeDataBundle:Mission');
+        $repo = $this->get('doctrine_mongodb')->getRepository('YourLifeDataBundle:Mission');
         $result = $this->convertMissionsToArray($repo->findAll());
 
         return new JsonResponse($result, 200);
     }
 
-    public function updateAction() {
-        return new JsonResponse();
-    }
+    public function createResultAction() {
 
-    public function createReportAction() {
-        return new JsonResponse();
-    }
-
-    /**
-     * Получаем доступные миссии для пользователя $user_id
-     * */
-    public function getAvailableMissionsAction() {
         $request = $this->getRequest();
 
         $user_id = $request->get('user_id');
-        $token = $request->get('token');
+        $token = $request->headers->get('session_token');;
 
-        $this->api_service->checkToken($token);
-        $user = $this->api_service->getUser($user_id);
+        $userByToken = $this->api_service->getUserByToken($token);
+        $userById = $this->api_service->getUserById($user_id);
 
-        $repo = $this->getDoctrine()->getRepository('YourLifeDataBundle:Mission');
+        if($userByToken != $userById)
+            throw new AccessErrorApiException();
 
-//        $missions = $repo->findBy([
-//            'user_level' => $user->getLevel()
-//        ]);
+        $mission_id = $request->get('mission_id');
+        $repo = $this->get('doctrine_mongodb')->getRepository('YourLifeDataBundle:Mission');
+        $mission = $repo->findOneBy([
+            'id' => $mission_id,
+            'user_level' => $userById->getId()
+        ]);
 
-        $result = $this->convertMissionsToArray($repo->findAll());
-        return new JsonResponse($result, 200);
+        if($mission == null)
+            throw new MissionNotFoundApiException();
+
+        $mission_title = $request->get('mission_title');
+        $points = $request->get('points');
+        $comment = $request->get('comment');
+        $status = $request->get('status');
+
+        $mission = new MissionResult();
+
+        $mission->setUser($userByToken);
+        $mission->setMission($mission);
+        $mission->setMissionTitle($mission_title);
+        $mission->setPoints($points);
+        $mission->setComment($comment);
+        $mission->setStatus($status);
+
+        /** @var MissionResultService $service */
+        $service = $this->get('your_life.data.mission_result_service');
+        try {
+            $service->create($mission);
+            return new JsonResponse(null, 201);
+        } catch(\Exception $ex) {
+            throw new ApiException(500, ApiExceptionType::ERROR_MISSION_RESULT_CREATE, $ex->getMessage());
+        }
+    }
+
+    public function addPhotoAction() {
+        return new JsonResponse();
+    }
+
+    public function updateResultAction() {
+        return new JsonResponse();
     }
 
     private function convertMissionsToArray($missions) {
         $result = [];
 
+        $path = $this->container->getParameter('yourlife.api.upload_photo_fullpath');
+
         /** @var Mission $mission */
         foreach($missions as $mission) {
             $resultPhotoList = [];
             $photoList = $mission->getPhotos();
-            /** @var MissionPhoto $photo */
+            /** @var Photo $photo */
             foreach($photoList as $photo) {
                 $resultPhotoList[] = [
                     'id' => $photo->getId(),
-                    'small' => $photo->getSmall(),
-                    'medium' => $photo->getMedium(),
-                    'origin' => $photo->getOrigin()
+                    'small' => sprintf('%s/%s', $path, $photo->getSmall()),
+                    'medium' => sprintf('%s/%s', $path, $photo->getMedium()),
+                    'origin' => sprintf('%s/%s', $path, $photo->getOrigin())
                 ];
             }
 
--- src/YourLife/ApiBundle/Tests/Controller/MissionsControllerTest.php
+++ src/YourLife/ApiBundle/Tests/Controller/MissionsControllerTest.php
@@ -3,10 +3,43 @@
 namespace YourLife\ApiBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 
 class MissionsControllerTest extends WebTestCase
 {
-    public function testGetMissions()
+    public function testCreateSessionToken()
     {
+        $client = $this->createClient();
+        $client->request(
+            'POST',
+            '/api/v1/sessions',
+            array(
+                'username' => 'test',
+                'password' => 'test'
+            ));
+
+        $arr = json_decode($client->getResponse()->getContent(), true);
+
+        return $arr;
+    }
+
+    /**
+     * @depends testCreateSessionToken
+     */
+    public function testGetMissions($params)
+    {
+        $client = $this->createClient();
+        $client->request(
+            'GET',
+            '/api/v1/users/'.$params['user_id'].'/missions',
+            array(
+                'username' => 'test',
+                'password' => 'test'
+            ),
+            array(),
+            array('HTTP_SESSION_TOKEN' => $params['token'])
+        );
+
+        print_r($client->getResponse()->getContent());
     }
 }
