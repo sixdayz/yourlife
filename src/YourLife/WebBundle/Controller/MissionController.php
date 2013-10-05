e4ed5d6785ccbac7d2b6e7ecfd558566278c4903
Pavel Patrin <pavelpat@ya.ru>
2013-10-05 12:11:33 +0000

    + Added mission controller

---
+++ src/YourLife/WebBundle/Controller/MissionController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace YourLife\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class MissionController
+ *
+ * @package YourLife\WebBundle\Controller
+ */
+class MissionController extends Controller
+{
+    /**
+     * @param $id
+     */
+    public function missionAllAction($id)
+    {
+
+    }
+
+    /**
+     * @param $id
+     */
+    public function missionShowAction($id)
+    {
+    }
+
+    /**
+     * @param $id
+     */
+    public function missionStartAction($id)
+    {
+    }
+
+    /**
+     * @param $id
+     */
+    public function missionSubmitAction($id)
+    {
+    }
+
+    /**
+     * @param $id
+     */
+    public function missionRejectAction($id)
+    {
+    }
+}
