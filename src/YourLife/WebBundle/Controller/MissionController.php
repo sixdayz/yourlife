7ae1d77502b2ceb42ce9870d03aa61c1993b40c1
Pavel Patrin <pavelpat@ya.ru>
2013-10-06 11:19:45 +0000

    ~ Client part updates

--- src/YourLife/WebBundle/Controller/MissionController.php
+++ src/YourLife/WebBundle/Controller/MissionController.php
@@ -5,6 +5,10 @@ namespace YourLife\WebBundle\Controller;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\DocumentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use YourLife\DataBundle\Document\Mission;
+use YourLife\DataBundle\Document\MissionResult;
+use YourLife\DataBundle\Enum\MissionResultStatus;
+use YourLife\DataBundle\Service\MissionResultService;
 
 /**
  * Class MissionController
@@ -24,10 +28,12 @@ class MissionController extends Controller
         /** @var DocumentRepository $missionRepository */
         $missionRepository = $documentManager->getRepository('YourLifeDataBundle:Mission');
 
+        $missions = $missionRepository->findAll();
+
         return $this->render(
             'YourLifeWebBundle:Mission:list.html.twig',
             [
-                'missions' => $missionRepository->findAll()
+                'missions' => $missions
             ]
         );
     }
@@ -35,35 +41,128 @@ class MissionController extends Controller
     /**
      *
      */
-    public function missionGetActiveAction() {
+    public function missionGetActiveAction()
+    {
+        /** @var DocumentManager $documentManager */
+        $documentManager = $this->get('doctrine_mongodb')->getManager('yourlife');
 
+        /** @var DocumentRepository $missionResultRepository */
+        $missionResultRepository = $documentManager->getRepository('YourLifeDataBundle:MissionResult');
+
+        $missionResults = $missionResultRepository->findBy(
+            [
+                'user' => $this->getUser()->getId(),
+                'status' => MissionResultStatus::IN_PROGRESS
+            ]
+        );
+
+        return $this->render(
+            'YourLifeWebBundle:Mission:active.html.twig',
+            [
+                'results' => $missionResults
+            ]
+        );
     }
 
     /**
      *
      */
-    public function missionGetHistoryAction() {
+    public function missionGetCompleteAction()
+    {
+        /** @var DocumentManager $documentManager */
+        $documentManager = $this->get('doctrine_mongodb')->getManager('yourlife');
 
+        /** @var DocumentRepository $missionResultRepository */
+        $missionResultRepository = $documentManager->getRepository('YourLifeDataBundle:MissionResult');
+
+        $missionResults = $missionResultRepository->findBy(
+            [
+                'user' => $this->getUser()->getId(),
+                'status' => MissionResultStatus::COMPLETE
+            ]
+        );
+
+        return $this->render(
+            'YourLifeWebBundle:Mission:complete.html.twig',
+            [
+                'results' => $missionResults
+            ]
+        );
     }
 
     /**
-     * @param $id
+     *
      */
-    public function missionStartAction($id)
+    public function missionGetCanceledAction()
     {
+        /** @var DocumentManager $documentManager */
+        $documentManager = $this->get('doctrine_mongodb')->getManager('yourlife');
+
+        /** @var DocumentRepository $missionResultRepository */
+        $missionResultRepository = $documentManager->getRepository('YourLifeDataBundle:MissionResult');
+
+        $missionResults = $missionResultRepository->findBy(
+            [
+                'user' => $this->getUser()->getId(),
+                'status' => MissionResultStatus::USER_CANCELED
+            ]
+        );
+
+        return $this->render(
+            'YourLifeWebBundle:Mission:canceled.html.twig',
+            [
+                'results' => $missionResults
+            ]
+        );
     }
 
-    /**
-     * @param $id
-     */
-    public function missionSubmitAction($id)
-    {
-    }
 
-    /**
-     * @param $id
-     */
-    public function missionRejectAction($id)
+    public function missionAction($id)
     {
+        /** @var DocumentManager $documentManager */
+        $documentManager = $this->get('doctrine_mongodb')->getManager('yourlife');
+
+        switch ($this->getRequest()->get('mission_result_action')) {
+            case "accept":
+                /** @var DocumentRepository $missionRepository */
+                $missionRepository = $documentManager->getRepository('YourLifeDataBundle:Mission');
+
+                /** @var Mission|null $mission */
+                $mission = $missionRepository->find($id);
+
+                /** @var MissionResultService $missionResultService */
+                $missionResultService = $this->get('your_life.data.mission_result_service');
+
+                $missionResult = new MissionResult();
+                $missionResult->setStatus(MissionResultStatus::IN_PROGRESS);
+                $missionResult->setUser($this->getUser());
+                $missionResult->setMission($mission);
+                $missionResult->setMissionTitle($mission->getTitle());
+
+                $missionResultService->create($missionResult);
+                break;
+            case "submit":
+                /** @var DocumentRepository $missionResultRepository */
+                $missionResultRepository = $documentManager->getRepository('YourLifeDataBundle:MissionResult');
+
+                /** @var MissionResult|null $missionResult */
+                $missionResult = $missionResultRepository->find($id);
+                $missionResult->setStatus(MissionResultStatus::COMPLETE);
+                $documentManager->persist($missionResult);
+                $documentManager->flush($missionResult);
+                break;
+            case "cancel":
+                /** @var DocumentRepository $missionResultRepository */
+                $missionResultRepository = $documentManager->getRepository('YourLifeDataBundle:MissionResult');
+
+                /** @var MissionResult|null $missionResult */
+                $missionResult = $missionResultRepository->find($id);
+                $missionResult->setStatus(MissionResultStatus::USER_CANCELED);
+                $documentManager->persist($missionResult);
+                $documentManager->flush($missionResult);
+                break;
+        }
+
+        return $this->redirect($this->generateUrl('your_life_web_mission_get_active'));
     }
 }
